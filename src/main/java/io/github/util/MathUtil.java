562703030a384b2d3f12f453c6d91d56ea9cd7fc
Shirish Pandharikar <shirish.pandharikar@gi-de.com>
2024-08-21 12:04:09 +0530

    first commit

---
+++ src/main/java/io/github/util/MathUtil.java
@@ -0,0 +1,13 @@
+package io.github.util;
+
+public class MathUtil {
+    private MathUtil() {}
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/test/java/io/github/util/MathUtilTest.java
@@ -0,0 +1,18 @@
+package io.github.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Math Utility Tests")
+class MathUtilTest {
+
+    @Test
+    @DisplayName("Adds 4 and 1")
+    void should_add_two_numbers() {
+        int actual = MathUtil.add(4, 1);
+        assertEquals(5, actual);
+    }
+
+}
